d6f39898ff7ec7e74d285da5ebee9d6114c0b813
ponspadova <padovapons@gmail.com>
2026-08-21 19:53:44 +0300

    Add files via upload

---
+++ particles.js
@@ -0,0 +1,194 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 280,
+      "density": {
+        "enable": true
+      }
+    },
+    "color": {
+      "value": ["#fff"]
+    },
+    "opacity": {
+      "value": 1,
+      "random": false,
+      "anim": {
+        "enable": true,
+        "speed": 0.4,
+        "opacity_min": 1,
+        "sync": false
+      }
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "size": {
+      "value": 2.5,
+      "random": true
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 220,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 1,
+      "random": true,
+      "direction": "none",
+      "straight": false
+    }
+  },
+  "interactivity": {
+    "detect_on": "window",
+    "events": {
+      "onhover": {
+        "enable": false
+      },
+      "resize": true
+    },
+    "modes": {
+      "push": {
+        "particles_nb": 1
+      }
+    }
+  }
+});
+
+(function enableMouseAttraction() {
+  var pointer = { x: null, y: null };
+  var attractionDistance = 180;
+
+  window.addEventListener("mousemove", function (event) {
+    pointer.x = event.clientX;
+    pointer.y = event.clientY;
+  });
+
+  window.addEventListener("mouseleave", function () {
+    pointer.x = null;
+    pointer.y = null;
+  });
+
+  function attractParticles() {
+    var particleSystem = window.pJSDom && window.pJSDom[0] && window.pJSDom[0].pJS;
+
+    if (particleSystem && pointer.x !== null) {
+      particleSystem.particles.array.forEach(function (particle) {
+        var dx = pointer.x - particle.x;
+        var dy = pointer.y - particle.y;
+        var distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance > 0 && distance < attractionDistance) {
+          var strength = (attractionDistance - distance) / attractionDistance * 0.012;
+          particle.x += dx * strength;
+          particle.y += dy * strength;
+        }
+      });
+    }
+
+    window.requestAnimationFrame(attractParticles);
+  }
+
+  attractParticles();
+})();
+
+/*
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 100,
+      "density": {
+        "enable": true,
+        "value_area":1000
+      }
+    },
+    "color": {
+      "value": ["#aa73ff", "#f8c210", "#83d238", "#33b1f8"]
+    },
+    
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#fff"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.6,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 2,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 120,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": false
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+*/
